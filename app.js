f65f840389ff16f4e26090e56e8fe807767fadc2
Danny Dwi Cahyono <dannydwicahyono@gmail.com>
2021-05-16 22:21:08 +0700

    adding-gitignore & setup app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
-const recipeRoute = require("./api/recipe/recipeRouter");
+const recipeRoute = require("./src/api/recipe/recipeRouter");
 
 app.use(cors());
 app.use(express.json());
@@ -16,14 +16,17 @@ app.get("/", (req, res) => {
 
 app.use("/recipe", recipeRoute);
 
-// const dbURI =
-//   "mongodb+srv://dannygg:ggwp123@nodeapp.la8gg.mongodb.net/recipe-management?retryWrites=true&w=majority";
-
+const port = process.env.PORT || 3000;
 const dbURI =
   "mongodb://dannygg:ggwp123@nodeapp-shard-00-00.la8gg.mongodb.net:27017,nodeapp-shard-00-01.la8gg.mongodb.net:27017,nodeapp-shard-00-02.la8gg.mongodb.net:27017/recipe-management?ssl=true&replicaSet=atlas-sdo70z-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(3000))
-  .catch((err) => console.log(err));
+  .then(() =>
+    app
+      .listen(port, () => {
+        console.log(`Listening on port ${port}`);
+      })
+      .catch((err) => console.log(err))
+  );
 console.log("Listening to port 3000");
