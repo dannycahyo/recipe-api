75b89a670bff02c7408e1b9631ca16fdda9623c5
Danny Dwi Cahyono <dannydwicahyono@gmail.com>
2021-05-26 17:23:28 +0700

    adding-env

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const recipeRoute = require("./src/api/recipe/recipeRouter");
+require("dotenv/config");
 
 app.use(cors());
 app.use(express.json());
@@ -17,8 +18,7 @@ app.get("/", (req, res) => {
 app.use("/recipe", recipeRoute);
 
 const port = process.env.PORT || 3000;
-const dbURI =
-  "mongodb://dannygg:ggwp123@nodeapp-shard-00-00.la8gg.mongodb.net:27017,nodeapp-shard-00-01.la8gg.mongodb.net:27017,nodeapp-shard-00-02.la8gg.mongodb.net:27017/recipe-management?ssl=true&replicaSet=atlas-sdo70z-shard-0&authSource=admin&retryWrites=true&w=majority";
+const dbURI = process.env.DB_URI;
 
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
